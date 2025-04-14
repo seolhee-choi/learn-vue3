de7e99e43f839d1bfe1190608282a58880c0df08
seolhee-choi <csheeee38@gmail.com>
2025-04-14 17:59:45 +0900

    first commit

---
+++ src/lectures/3/3/App_CompositionAPI.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<button v-on:click="increment">Counter: {{ counter }}</button>
+	</div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+
+export default {
+	setup() {
+		const counter = ref(0);
+
+		const increment = () => {
+			counter.value++;
+		};
+
+		onMounted(() => {
+			console.log('컴포넌트 마운트됨');
+		});
+
+		return {
+			counter,
+			increment,
+		};
+	},
+};
+</script>
+
+<style></style>
---
+++ src/lectures/3/3/App_OptionsAPI.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<button v-on:click="increment">Counter: {{ counter }}</button>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			counter: 0,
+		};
+	},
+	methods: {
+		increment() {
+			this.counter++;
+		},
+	},
+	mounted() {
+		console.log('컴포넌트 마운트됨');
+	},
+};
+</script>
+
+<style></style>
